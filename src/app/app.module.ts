df6d55d38f14a92e97459edacebd9ce8f4e8d475
thepsalmist <xf.xavierfrank@gmail.com>
2019-05-31 16:30:55 +0300

    create a quote form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule
