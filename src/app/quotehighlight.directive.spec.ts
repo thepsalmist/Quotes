33b2959fd169ab8931d062fe963758e2e9aa8c9f
thepsalmist <xf.xavierfrank@gmail.com>
2019-06-03 00:45:48 +0300

    Auto-generated commit

--- src/app/quotehighlight.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { QuotehighlightDirective } from './quotehighlight.directive';
-
-describe('QuotehighlightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new QuotehighlightDirective();
-    expect(directive).toBeTruthy();
-  });
-});
